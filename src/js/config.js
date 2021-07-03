70a4c1d24fc5ea4c556af4c0a267a3a6176475b4
Pratham Pancholi <pancholipratham33@gmail.com>
2021-07-03 13:34:57 +0530

    removed api key bug

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIME_OUT = 10;
 export const RESULTS_PER_PAGE = 10;
-export const KEY='3e934dfa-22ae-461c-b1f9-aa37a8890507';
-export const MODAL_CLOSE=2500;
+export const KEY = '88bec14f-69cf-4593-809b-25b3bd6c6623';
+export const MODAL_CLOSE = 2500;
